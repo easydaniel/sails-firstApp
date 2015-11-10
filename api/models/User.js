a3ce05062b07b2f138c299a650a639ccd7f6b019
DanielChien <whitetiger50512@gmail.com>
2015-11-10 12:58:09 +0800

    init

---
+++ api/models/User.js
@@ -0,0 +1,45 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+    schema: true,
+
+
+    attributes: {
+
+        name: {
+
+            type: 'string',
+            required: true
+        },
+
+        email: {
+
+            type: 'email',
+            required: true,
+            unique: true
+
+        },
+
+        encryptedPassword: {
+
+            type: 'string',
+            required: true
+
+        },
+
+        toJSON: function() {
+            var obj = this.toObject();
+            delete obj._csrf;
+            delete obj.confirmPasswd;
+            return obj;
+        }
+
+    }
+};
+
