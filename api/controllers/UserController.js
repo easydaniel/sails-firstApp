683a7da241384d41620f0c5885dd35e729d82c6d
DanielChien <whitetiger50512@gmail.com>
2015-11-10 20:12:45 +0800

    Add user CRUD

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -12,13 +12,96 @@ module.exports = {
     },
 
     create: function(req, res, next) {
-        User.create( req.params.all(), function userCreated(err, user) {
+        User.create(req.params.all(), function userCreated(err, user) {
             if (err) {
                 console.log(err);
+                
+                req.session.flash = {
+                    err: err
+                }
                 return res.redirect('/user/new');
             }
-            res.json(user);
+            //res.json(user);
+            res.redirect('/user/show/' + user.id);
         });
+    },
+
+    show: function(req, res, next) {
+        User.findOne(req.params.id, function foundUser(err, user) {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                return next();
+            }
+            res.view({
+                user: user
+            });
+        });
+    },
+
+    edit: function(req, res, next) {
+        User.findOne(req.params.id, function foundUser(err, user) {
+           if (err) {
+              return next(err);
+           }
+            if (!user) {
+               return next();
+            }
+           res.view({
+               user: user
+           }); 
+        });
+    },
+
+    update: function(req, res, next) {
+
+        User.update(req.params.id, req.params.all(), function userUpdate(err) {
+            if (err) {
+                return res.redirect('/user/edit/' + req.params.id);
+            }
+
+            res.redirect('/user/show/' + req.params.id);
+
+        });
+
+    },
+
+    destroy: function(req, res, next) {
+        
+        User.findOne(req.params.id, function foundUser(err, user) {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                return next();
+            }
+
+            User.destroy(req.params.id, function userDestroy(err) {
+                if (err) {
+                    return next(err);
+                }
+            });
+
+            res.redirect('/user');
+
+        });
+
+
+    },
+
+    index: function(req, res, next) {
+        
+        User.find(function foundUsers(err, users) {
+            if (err) {
+                return next(err);
+            }
+            res.view({
+                users: users
+            });
+        });
+
+
     }
 	
 };
--- api/models/User.js
+++ api/models/User.js
@@ -29,7 +29,6 @@ module.exports = {
         encryptedPassword: {
 
             type: 'string',
-            required: true
 
         },
 
@@ -39,6 +38,23 @@ module.exports = {
             delete obj.confirmPasswd;
             return obj;
         }
+
+    },
+
+    beforeCreate: function(values, next) {
+        if (!values.password || values.password != values.confirmPasswd) {
+            return next({
+                err: 'Password does not match'
+            });
+        }
+
+        require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encrypted) {
+            if (err) {
+                return next(err);
+            }
+            values.encryptedPassword = encrypted;
+            next();
+        });
 
     }
 };
